f4f86fbb0b91a7b3a9344d6c0c106a10385b7853
Dunghoang91 <dung509b@gmai.com>
2018-09-08 20:21:53 +0700

    shop grid/list

--- wp-content/themes/mocha/woocommerce/content-product.php
+++ wp-content/themes/mocha/woocommerce/content-product.php
@@ -29,19 +29,17 @@ if ( ! $product || ! $product->is_visible() ) {
 					 * @hooked woocommerce_show_product_loop_sale_flash - 10
 					 * @hooked woocommerce_template_loop_product_thumbnail - 10
 					 */
-					do_action( 'woocommerce_before_shop_loop_item_title' );
-					
-				?>
-			</div>
-			<div class="item-content products-content">
-			<?php
-				/**
-				 * woocommerce_shop_loop_item_title hook
-				 *
-				 * @hooked woocommerce_template_loop_product_title - 10
-				 */
-				do_action( 'woocommerce_shop_loop_item_title' );
-
+					do_action( 'woocommerce_before_shop_loop_item_title' );					
+					?>
+				</div>
+				<div class="item-content products-content">
+					<?php
+					/**
+					 * woocommerce_shop_loop_item_title hook
+					 *
+					 * @hooked woocommerce_template_loop_product_title - 10
+					 */
+					do_action( 'woocommerce_shop_loop_item_title' );
 				/**
 				 * woocommerce_after_shop_loop_item_title hook
 				 *
@@ -49,15 +47,15 @@ if ( ! $product || ! $product->is_visible() ) {
 				 * @hooked woocommerce_template_loop_price - 10
 				 */
 				do_action( 'woocommerce_after_shop_loop_item_title' );
-			?>
-			<?php
+				?>
+				<?php
 				/**
 				 * woocommerce_after_shop_loop_item hook
 				 *
 				 * @hooked woocommerce_template_loop_add_to_cart - 10
 				 */
 				do_action( 'woocommerce_after_shop_loop_item' );
-			?>
+				?>
 			</div>
 		</div>
 	</div>
