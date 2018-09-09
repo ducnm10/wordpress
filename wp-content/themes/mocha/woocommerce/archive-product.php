9c0a81a2403ac8eb756b43828c816431a82ae84f
Dunghoang91 <dung509b@gmai.com>
2018-09-09 12:41:11 +0700

    shop menu

--- wp-content/themes/mocha/woocommerce/archive-product.php
+++ wp-content/themes/mocha/woocommerce/archive-product.php
@@ -32,6 +32,16 @@
 <?php endif; ?>
 <div class="container">
 	<div class="row">
+	<?php 	
+	if ( is_active_sidebar('left-product') && mocha_sidebar_product() != 'right' && mocha_sidebar_product() != 'full' ):
+		$mocha_left_span_class = 'col-lg-'.mocha_options()->getCpanelValue('sidebar_left_expand');
+		$mocha_left_span_class .= ' col-md-'.mocha_options()->getCpanelValue('sidebar_left_expand_md');
+		$mocha_left_span_class .= ' col-sm-'.mocha_options()->getCpanelValue('sidebar_left_expand_sm');
+	?>
+	<aside id="left" class="sidebar <?php echo esc_attr($mocha_left_span_class); ?>">
+		<?php dynamic_sidebar('left-product'); ?>
+	</aside>	
+	<?php endif; ?>
 	<div id="contents" <?php mocha_content_product(); ?> role="main">
 		<?php
 			/**
@@ -104,19 +114,7 @@
 			 */
 			do_action('woocommerce_after_main_content');
 		?>
-	</div>
-<?php 
-	
-	if ( is_active_sidebar('left-product') && mocha_sidebar_product() != 'right' && mocha_sidebar_product() != 'full' ):
-		$mocha_left_span_class = 'col-lg-'.mocha_options()->getCpanelValue('sidebar_left_expand');
-		$mocha_left_span_class .= ' col-md-'.mocha_options()->getCpanelValue('sidebar_left_expand_md');
-		$mocha_left_span_class .= ' col-sm-'.mocha_options()->getCpanelValue('sidebar_left_expand_sm');
-	?>
-	<aside id="left" class="sidebar <?php echo esc_attr($mocha_left_span_class); ?>">
-		<?php dynamic_sidebar('left-product'); ?>
-	</aside>
-	
-	<?php endif; ?>
+	</div>	
 	<?php if ( is_active_sidebar('right-product') && mocha_sidebar_product() != 'left' && mocha_sidebar_product() != 'full' ):
 		$mocha_right_span_class = 'col-lg-'.mocha_options()->getCpanelValue('sidebar_right_expand');
 		$mocha_right_span_class .= ' col-md-'.mocha_options()->getCpanelValue('sidebar_right_expand_md');
